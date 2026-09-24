322ad833e9bb7221a61782d03eb385864f84b037
Ilham Kukuh Prakosa <56493086+ilham89@users.noreply.github.com>
2026-09-24 17:10:47 +0700

    workshop: remove duplicate sandbox implementation

--- src/features/workshop/composition.client.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import { workshopApi } from "./infrastructure/workshop-api";
-import { WorkshopStarter } from "./presentation/workshop-starter";
-
-export function WorkshopFeature() {
-  return <WorkshopStarter api={workshopApi} />;
-}
